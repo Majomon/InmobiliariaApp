c51b768c6eb8440a96240e47c2201f107a24c6cf
Mauri <mauri.monzon91@gmail.com>
2023-10-03 19:01:06 -0300

    Renegando con las imagenes SVG

--- client/src/components/DetailInfoTop/DetailInfoTop.jsx
+++ client/src/components/DetailInfoTop/DetailInfoTop.jsx
@@ -1,10 +1,12 @@
 import PropTypes from "prop-types";
 import { HiArrowNarrowRight } from "react-icons/hi";
-import Superficie from "../../assets/superficie.png";
+import Superficie from "../../assets/superficie.svg";
+import Ambientes from "../../assets/ambientes.svg";
+import Baño from "../../assets/bano.svg";
 
 function DetailInfoTop({ dataAxios }) {
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full bg-gray-100">
       {/* Info del vendedor o inmobiliaria */}
       <div className="w-full h-[3.5rem] flex justify-end gap-6 px-10">
         <div className="flex justify-center items-center">
@@ -70,8 +72,8 @@ function DetailInfoTop({ dataAxios }) {
         </div>
       </div>
       {/* Ubicación */}
-      <div className="w-full h-full flex gap-2 mx-auto my-2 px-10">
-        <div className="flex items-center gap-2">
+      <div className="w-full h-full flex gap-2 mx-auto">
+        <div className="flex items-center gap-2 pl-16">
           <h3>{dataAxios.operation}</h3>
           <HiArrowNarrowRight />
         </div>
@@ -88,8 +90,8 @@ function DetailInfoTop({ dataAxios }) {
         </div>
       </div>
       {/* Info extra del la publicacion */}
-      <div className="w-full h-full mx-auto flex">
-        <div className="w-8/12 h-[7rem] px-10 py-2 flex items-center bg-red-400">
+      <div className="w-full h-full mx-auto flex justify-center items-center bg-white shadow-md">
+        <div className="w-7/12 h-[7rem] flex pl-16 py-4">
           <div className="w-6/12 h-full">
             <h3>{dataAxios.name}</h3>
             <h4>
@@ -114,66 +116,24 @@ function DetailInfoTop({ dataAxios }) {
             )}
           </div>
         </div>
-        <div className="flex flex-col w-4/12 h-full bg-yellow-500">
-          <div>
-            <img
-              src={Superficie}
-              alt="Superficice"
-              className="w-[60px] h-[60px]"
-            />
-            <strong>{dataAxios.area}</strong>
-            <span>Sup. Total</span>
+        <div className="h-[5rem] border-l border-gray-400">
+
+        </div>
+        <div className="w-5/12 h-[7rem] flex">
+          <div className="w-[25%] flex flex-col justify-center items-center pl-4">
+            <img src={Superficie} alt="Superficice" className="w-[25px]" />
+            <strong className="text-lg">{dataAxios.area} m²</strong>
+            <span className="text-sm">Sup. Total</span>
           </div>
-          <div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="icon icon-tabler icon-tabler-bed-filled"
-              width="60"
-              height="60"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="#2c3e50"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path
-                d="M3 6a1 1 0 0 1 .993 .883l.007 .117v6h6v-5a1 1 0 0 1 .883 -.993l.117 -.007h8a3 3 0 0 1 2.995 2.824l.005 .176v8a1 1 0 0 1 -1.993 .117l-.007 -.117v-3h-16v3a1 1 0 0 1 -1.993 .117l-.007 -.117v-11a1 1 0 0 1 1 -1z"
-                stroke-width="0"
-                fill="currentColor"
-              />
-              <path
-                d="M7 8a2 2 0 1 1 -1.995 2.15l-.005 -.15l.005 -.15a2 2 0 0 1 1.995 -1.85z"
-                stroke-width="0"
-                fill="currentColor"
-              />
-            </svg>
-            <strong>{dataAxios.environments}</strong>
-            <span>Ambientes</span>
+          <div className="w-[25%] flex flex-col justify-center items-center">
+            <img src={Ambientes} alt="Ambientes" className="w-[25px]" />
+            <strong className="text-lg">{dataAxios.environments}</strong>
+            <span className="text-sm">Ambientes</span>
           </div>
-          <div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="icon icon-tabler icon-tabler-bath-filled"
-              width="60"
-              height="60"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="#2c3e50"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path
-                d="M11 2a1 1 0 0 1 .993 .883l.007 .117v2.25a1 1 0 0 1 -1.993 .117l-.007 -.117v-1.25h-2a1 1 0 0 0 -.993 .883l-.007 .117v6h13a2 2 0 0 1 1.995 1.85l.005 .15v3c0 1.475 -.638 2.8 -1.654 3.715l.486 .73a1 1 0 0 1 -1.594 1.203l-.07 -.093l-.55 -.823a4.98 4.98 0 0 1 -1.337 .26l-.281 .008h-10a4.994 4.994 0 0 1 -1.619 -.268l-.549 .823a1 1 0 0 1 -1.723 -1.009l.059 -.1l.486 -.73a4.987 4.987 0 0 1 -1.647 -3.457l-.007 -.259v-3a2 2 0 0 1 1.85 -1.995l.15 -.005h1v-6a3 3 0 0 1 2.824 -2.995l.176 -.005h3z"
-                stroke-width="0"
-                fill="currentColor"
-              />
-            </svg>
-            <strong>{dataAxios.bathrooms}</strong>
-            <span>Baños</span>
+          <div className="w-[25%] flex flex-col justify-center items-center">
+            <img src={Baño} alt="Baños" className="w-[25px] text-red-500" />
+            <strong className="text-lg">{dataAxios.bathrooms}</strong>
+            <span className="text-sm">Baños</span>
           </div>
         </div>
       </div>
