e302ee7b4b847f0ce215af642caecf3411b5807d
Mauri <mauri.monzon91@gmail.com>
2023-10-09 22:09:18 -0300

    Filtro con estado global

--- client/src/components/Filter/Filter.jsx
+++ client/src/components/Filter/Filter.jsx
@@ -1,20 +1,25 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
 
-function Filter({ dataAxios }) {
-  const [operation, setOperation] = useState([]);
-  const [typeProperty, setTypeProperty] = useState([]);
-  const [province, setProvince] = useState([]);
-  const [zone, setZone] = useState([]);
-  const [bedrooms, setBedrooms] = useState([]);
-  
+function Filter() {
+  const properties = useSelector((state) => state.propiedades);
   const [orderData, setOrderData] = useState({
     operation: "",
     typeProperty: "",
     province: "",
     zone: "",
     bedrooms: "",
-    orderData: "",
   });
+
+  const uniqueOperations = [
+    ...new Set(properties.map((prop) => prop.operation)),
+  ];
+  const uniqueTypes = [...new Set(properties.map((prop) => prop.property))];
+  const uniqueProvinces = [
+    ...new Set(properties.map((prop) => prop.address.province)),
+  ];
+  const uniqueZones = [...new Set(properties.map((prop) => prop.address.zone))];
+  const uniqueBedrooms = [...new Set(properties.map((prop) => prop.bedrooms))];
 
   return (
     <div className="w-full absolute bottom-0 bg-black opacity-70">
@@ -23,50 +28,95 @@ function Filter({ dataAxios }) {
           <label className="block text-sm font-medium text-white mb-1 opacity-100">
             Operación
           </label>
-          <select className="block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-indigo-200 focus:border-indigo-300 opacity-100">
+          <select
+            className="block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-indigo-200 focus:border-indigo-300 opacity-100"
+            value={orderData.operation}
+            onChange={(e) =>
+              setOrderData({ ...orderData, operation: e.target.value })
+            }
+          >
             <option value="all">Operación</option>
-            <option value="A">Alquiler</option>
-            <option value="B">Venta</option>
+            {uniqueOperations.map((op) => (
+              <option key={op} value={op}>
+                {op}
+              </option>
+            ))}
           </select>
         </div>
         <div className="w-2/12 py-2">
           <label className="block text-sm font-medium text-white mb-1">
             Tipo
           </label>
-          <select className="block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-indigo-200 focus:border-indigo-300">
-            <option value="all">Tipo</option>
-            <option value="C">Casa</option>
-            <option value="D">Departamento</option>
+          <select
+            className="block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-indigo-200 focus:border-indigo-300 opacity-100"
+            value={orderData.uniqueTypes}
+            onChange={(e) =>
+              setOrderData({ ...orderData, operation: e.target.value })
+            }
+          >
+            <option value="all">Operación</option>
+            {uniqueTypes.map((op) => (
+              <option key={op} value={op}>
+                {op}
+              </option>
+            ))}
           </select>
         </div>
         <div className="w-2/12 py-2">
           <label className="block text-sm font-medium text-white mb-1">
             Zona
           </label>
-          <select className="block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-indigo-200 focus:border-indigo-300">
-            <option value="all">Zona</option>
-            <option value="E">E</option>
-            <option value="F">F</option>
+          <select
+            className="block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-indigo-200 focus:border-indigo-300 opacity-100"
+            value={orderData.uniqueProvinces}
+            onChange={(e) =>
+              setOrderData({ ...orderData, operation: e.target.value })
+            }
+          >
+            <option value="all">Operación</option>
+            {uniqueProvinces.map((op) => (
+              <option key={op} value={op}>
+                {op}
+              </option>
+            ))}
           </select>
         </div>
         <div className="w-2/12 py-2">
           <label className="block text-sm font-medium text-white mb-1">
             Barrio
           </label>
-          <select className="block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-indigo-200 focus:border-indigo-300">
+          <select
+            className="block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-indigo-200 focus:border-indigo-300 opacity-100"
+            value={orderData.uniqueZones}
+            onChange={(e) =>
+              setOrderData({ ...orderData, operation: e.target.value })
+            }
+          >
             <option value="all">Barrio</option>
-            <option value="G">G</option>
-            <option value="H">H</option>
+            {uniqueZones.map((op) => (
+              <option key={op} value={op}>
+                {op}
+              </option>
+            ))}
           </select>
         </div>
         <div className="w-2/12 py-2">
           <label className="block text-sm font-medium text-white mb-1">
             Ambientes
           </label>
-          <select className="block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-indigo-200 focus:border-indigo-300">
+          <select
+            className="block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-indigo-200 focus:border-indigo-300 opacity-100"
+            value={orderData.uniqueBedrooms}
+            onChange={(e) =>
+              setOrderData({ ...orderData, operation: e.target.value })
+            }
+          >
             <option value="all">Ambientes</option>
-            <option value="I">I</option>
-            <option value="J">J</option>
+            {uniqueBedrooms.map((op) => (
+              <option key={op} value={op}>
+                {op}
+              </option>
+            ))}
           </select>
         </div>
         <div className="w-full">
