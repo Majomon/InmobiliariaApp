7142707f592cd59923854fd4e8096d696b9d9a10
Mauri <mauri.monzon91@gmail.com>
2023-11-30 23:23:00 -0300

    Menu

--- client/src/components/DashBoard/MenuLatelDashBoard/MenuLatelDashBoard.jsx
+++ client/src/components/DashBoard/MenuLatelDashBoard/MenuLatelDashBoard.jsx
@@ -3,7 +3,7 @@ function MenuLatelDashBoard() {
   return (
     <ul className="w-2/12 min-h-screen border-r-2 flex flex-col pl-10 pt-10 gap-4">
       {properties.map((elem) => (
-        <li className="text-left">{elem}</li>
+        <li className="cursor-pointer">{elem}</li>
       ))}
     </ul>
   );
--- client/src/components/DashBoard/opcionesMenuLateral.js
+++ client/src/components/DashBoard/opcionesMenuLateral.js
@@ -2,6 +2,8 @@ const properties = [
   "Inicio",
   "P. habilitadas",
   "P. inhabilitadas",
+  "Agregar propiedad",
+  "Modificar propiedad",
 ];
 
 export default properties;
