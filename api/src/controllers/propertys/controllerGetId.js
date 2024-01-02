adbf6c9862822e684f861e998cc9659655e88e7e
Mauricio Monzon <104470486+Majomon@users.noreply.github.com>
2024-01-02 12:37:23 -0300

    act

--- api/src/controllers/propertys/controllerGetId.js
+++ api/src/controllers/propertys/controllerGetId.js
@@ -2,7 +2,6 @@ const Property = require("../../models/properties");
 
 const controllerGetId = async (id) => {
   const property = await Property.findById(id)
-  console.log(property);
   return property;
 };
 
