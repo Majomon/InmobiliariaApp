551e49efeb6eb91543255caa44653f3274e5fa81
Mauricio Monzon <104470486+Majomon@users.noreply.github.com>
2024-01-07 18:44:47 -0300

    Cantidad de img permitidas: 12

--- api/src/routes/cloudinary.routes.js
+++ api/src/routes/cloudinary.routes.js
@@ -3,6 +3,6 @@ const parser = require("../utils/cloudinary");
 const cloudinaryRouter = require("express").Router();
 
 // Usar el parser como un middleware en la ruta POST
-cloudinaryRouter.post("/", parser.array("img", 10), handlerPostCloudinary);
+cloudinaryRouter.post("/", parser.array("img", 12), handlerPostCloudinary);
 
 module.exports = cloudinaryRouter;
