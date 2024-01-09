fcfd8d7861c5a607b1768861979dbf41fae5fef1
Mauricio Monzon <104470486+Majomon@users.noreply.github.com>
2024-01-09 14:11:41 -0300

    Actualizacion de cors

--- api/src/app.js
+++ api/src/app.js
@@ -8,13 +8,33 @@ const routerSearch = require("./routes/search.routes");
 const routerCloudinary = require("./routes/cloudinary.routes");
 const routerFirebase = require("./routes/firebase.routes");
 
-
 //Rutas
 
 const app = express();
 
 // Habilitar CORS usando el middleware 'cors'
-app.use(cors());
+const corsOptions = {
+  origin: "https://inmobiliaria-client.vercel.app", 
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+// Habilitar CORS usando el middleware 'cors'
+app.use(cors(corsOptions));
+
+// Middlewares
+app.use(express.json());
+app.use(morgan("dev"));
+
+// Routes
+app.use("/users", routerUser);
+app.use("/properties", routerProperty);
+app.use("/resend", routerResend);
+app.use("/search", routerSearch);
+app.use("/cloudinary", routerCloudinary);
+app.use("/firebase", routerFirebase);
+
+module.exports = app;
 
 // Middleware para configurar los encabezados CORS
 /* app.use((req, res, next) => {
@@ -27,17 +47,3 @@ app.use(cors());
   next();
 });
  */
-//Middlewares
-app.use(express.json());
-app.use(morgan("dev"));
-
-// Routes
-app.use("/users", routerUser);
-app.use("/properties", routerProperty);
-app.use("/resend", routerResend);
-app.use("/search", routerSearch);
-app.use("/cloudinary", routerCloudinary);
-app.use("/firebase", routerFirebase);
-
-
-module.exports = app;
